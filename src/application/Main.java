95df40bc0a4ad1b6e1dd99e3dbb655ba4a1e0bd3
BatistaQ <kauanbs29@hotmail.com>
2025-01-13 23:48:52 -0300

    writing files

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import entities.Data;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +16,7 @@ public class Main {
             Scanner sc = new Scanner(System.in);
 
             String line;
-            ArrayList<String> dataArray = new ArrayList<>();
+            List<String> dataArray = new ArrayList<>();
 
             while((line =bufferedReader.readLine()) != null){
                 System.out.println(line);
@@ -27,16 +28,39 @@ public class Main {
             int age = Integer.parseInt(dataArray.get(2));
             double height = Double.parseDouble(dataArray.get(3));
 
+            //Data data = new Data(name,email,age,height);
+
             Data data = new Data(name,email,age,height);
+            ArrayList<Data> datas = new ArrayList<>();
+            datas.add(data);
+
 
             System.out.println();
             System.out.println(name);
             System.out.println(email);
             System.out.println(age);
             System.out.println(height);
+
+            String formattedName = data.getName().replace(" ","").toUpperCase();
+            String outputFile = datas.indexOf(data) + 1 + "-" + formattedName + ".txt";
+            System.out.println(outputFile);
+
+            try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))){
+                bufferedWriter.write("Nome:" + data.getName());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Email:" + data.getEmail());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Idade:" + data.getAge());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Altura:" + data.getHeight());
+            } catch (IOException e){
+                System.out.println("Erro ao acessar o arquivo: " + e.getMessage());
+            }
+
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
+
     }
 
     private static String validateInput(String line, Scanner sc) {
